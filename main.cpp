b8904d155e87314bc1da01912029c8639ff1290e
Vicki Lee <vlee088@gmail.com>
2017-08-19 12:19:27 -0500

    added comments

--- main.cpp
+++ main.cpp
@@ -36,32 +36,33 @@ int getDecLetter(char l){
     return 0;
 }
 
+//ConvertToDec function takes a string number (binary or hexadecimel) and returns an integer value
 int ConvertToDec(string num, int power){
     int acc = 0;
     if(power == 2){
         for(int i = 0; i < num.length(); i++){
-            int bnum = (int)num[i] - '0';
-            acc += (pow(power, (num.length() - 1) - i)) * bnum;
+            int bin_num = (int)num[i] - '0';
+            acc += (pow(power, (num.length() - 1) - i)) * bin_num;
         }
     }
 
     if(power == 16){
         for(int i = 0; i < num.length(); i++){
-            int hnum;
+            int hex_num;
             if(isdigit(num[i])){
-                hnum = (int)num[i] - '0';
+                hex_num = (int)num[i] - '0';
             }
             else if(isalpha(num[i])){
-                hnum = getDecLetter(num[i]);
+                hex_num = getDecLetter(num[i]);
 
             }
-            acc += (pow(power, (num.length() - 1) - i)) * hnum;
+            acc += (pow(power, (num.length() - 1) - i)) * hex_num;
         }
     }
     return acc;
 }
 
-
+//DecToBin function takes a decimal int number and returns a binary string number
 string DecToBin(int dec_num){
     string answer;
     int remainder;
@@ -88,8 +89,7 @@ string DecToBin(int dec_num){
     return reverseNum(answer);
 }
 
-
-
+//DecToHex function takes a decimal int number and returns a hexadecimal string number
 string DecToHex(int dec_num){
     string answer;
     if(dec_num > 15){
@@ -111,32 +111,35 @@ string DecToHex(int dec_num){
             answer.push_back(getHexLetter(dec_num));
             return answer;
         }
-        else{
-            return to_string(dec_num);
-        }
     }
+    return to_string(dec_num);
 }
 
+//BinToDec function takes a binary string number, converts it to decimal and returns an integer value
 int BinToDec(string binary_num){
     return ConvertToDec(binary_num, 2);
 }
 
+//HexToDec function takes a hexadecimal string number, converts it to decimal and returns an integer value
 int HexToDec(string hex_num){
     return ConvertToDec(hex_num, 16);
 }
 
+//BinToHex function takes a binary string number, converts it to decimal then to hexadecimal, and returns a string number
 string BinToHex(string binary_num){
     int dec = BinToDec(binary_num);
     return DecToHex(dec);
 
 }
 
+//HexToBin function takes a hex string number, converts it to decimal then to binary, and returns a string number
 string HexToBin(string hex_num){
     int dec = HexToDec(hex_num);
     return DecToBin(dec);
 }
 
-bool checkBinary(string input){
+//CheckBinary function checks binary input for numbers other than 1 and 0, returns false if found, otherwise true
+bool CheckBinary(string input){
     for(int i = 0; i < input.length(); i++){
         int digit = (int)input[i] - '0';
         if(digit != 0 && digit != 1)
@@ -145,25 +148,26 @@ bool checkBinary(string input){
     return true;
 }
 
-bool checkNegative(string input){
+//CheckNegative function checks input number for negative sign, returns false if found, otherwise true
+bool CheckNegative(string input){
     if(input[0] == '-')
         return false;
     return true;
 }
 
-
+//CheckStrInput function takes the command, checks for errors and returns empty if error, otherwise returns str_input
 string CheckStrInput(int command){
     cout << "Please enter a number: ";
     string str_input;
     cin >> str_input;
     cout << endl;
-    if(!checkNegative(str_input)){
+    if(!CheckNegative(str_input)){
         cout << "Error: no negative values. Return to Main Menu." << endl;
         return "";
     }
 
     if(command < 2){
-        bool flag = checkBinary(str_input);
+        bool flag = CheckBinary(str_input);
         if(!flag){
             cout << "Flag: " << flag << " Error: incorrect binary format." << endl;
             return "";
@@ -173,6 +177,7 @@ string CheckStrInput(int command){
     return str_input;
 }
 
+//CheckIntInput function takes command input, checks for error, if found return 0, else returns num_input 
 int CheckIntInput(int command){
     cout << "Please enter a number: ";
     int num_input;
@@ -180,11 +185,12 @@ int CheckIntInput(int command){
     cout << endl;
     if(num_input < 0){
         cout << "Error: no negative values." << endl;
-        return -1;
+        return 0;
     }
     return num_input;
 }
 
+//ExecuteCommand function takes command input, finds the function matching command, and returns result
 void ExecuteCommand(int command){
     string result = "";
     switch(command){
@@ -230,6 +236,7 @@ void ExecuteCommand(int command){
     cout << "\n" << result << endl;
 }
 
+//MenuCommand function shows the menu and sends command input to ExecuteCommand function
 void MenuCommand(){
     int command = 0;
     while(command != 6){
