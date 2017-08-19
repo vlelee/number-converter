6cdfcde4450b4b1f16ce7d8a7bea4a405a895e90
Vicki Lee <vlee088@gmail.com>
2017-08-19 14:48:23 -0500

    added more error checks

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <limits>
 #include <math.h>
 
 using namespace std;
@@ -20,6 +21,12 @@ string to_string(int num){
     return convert.str();
 }
 
+string to_string(float num){
+    ostringstream convert;
+    convert << num;
+    return convert.str();
+}
+
 char getHexLetter(int num){
     char letter[6] = {'A', 'B', 'C', 'D', 'E', 'F'};
     return letter[num - 10];
@@ -32,31 +39,34 @@ int getDecLetter(char l){
             return 10 + i;
         }
     }
-    cout << "Incorrect Hex Letter found. Results will be incorrect." << endl;
-    return 0;
+    cout << "Error: Incorrect Hex Letter." << endl;
+    return -1;
 }
 
 //ConvertToDec function takes a string number (binary or hexadecimel) and returns an integer value
 int ConvertToDec(string num, int power){
     int acc = 0;
+    if(num == "-1"){
+        return -1;
+    }
     if(power == 2){
         for(int i = 0; i < num.length(); i++){
-            int bin_num = (int)num[i] - '0';
-            acc += (pow(power, (num.length() - 1) - i)) * bin_num;
+            int bnum = (int)num[i] - '0';
+            acc += (pow(power, (num.length() - 1) - i)) * bnum;
         }
     }
 
     if(power == 16){
         for(int i = 0; i < num.length(); i++){
-            int hex_num;
+            int hnum;
             if(isdigit(num[i])){
-                hex_num = (int)num[i] - '0';
+                hnum = (int)num[i] - '0';
             }
             else if(isalpha(num[i])){
-                hex_num = getDecLetter(num[i]);
+                hnum = getDecLetter(num[i]);
 
             }
-            acc += (pow(power, (num.length() - 1) - i)) * hex_num;
+            acc += (pow(power, (num.length() - 1) - i)) * hnum;
         }
     }
     return acc;
@@ -67,7 +77,8 @@ string DecToBin(int dec_num){
     string answer;
     int remainder;
     if(dec_num < 0) {
-        return "No negative decimal numbers";
+        cout << "Error: input error." << endl;
+        return "-1";
     }
 
     while(dec_num > 0){
@@ -115,6 +126,7 @@ string DecToHex(int dec_num){
     return to_string(dec_num);
 }
 
+
 //BinToDec function takes a binary string number, converts it to decimal and returns an integer value
 int BinToDec(string binary_num){
     return ConvertToDec(binary_num, 2);
@@ -139,7 +151,7 @@ string HexToBin(string hex_num){
 }
 
 //CheckBinary function checks binary input for numbers other than 1 and 0, returns false if found, otherwise true
-bool CheckBinary(string input){
+bool checkBinary(string input){
     for(int i = 0; i < input.length(); i++){
         int digit = (int)input[i] - '0';
         if(digit != 0 && digit != 1)
@@ -149,57 +161,64 @@ bool CheckBinary(string input){
 }
 
 //CheckNum function checks input number for negative sign or decimal, returns false if found, otherwise true
-bool CheckNum(string input){
-    if(input.find("-") || input.find("."))
+bool checkNum(string input){
+    size_t foundneg = input.find("-");
+    size_t founddec = input.find(".");
+
+    if(foundneg !=string::npos || founddec != string::npos){
         return false;
+    }
     return true;
 }
 
-//CheckStrInput function takes the command, checks for errors and returns empty if error, otherwise returns str_input
+//CheckStrInput function takes the command, checks for errors and returns "-1" if error, otherwise returns str_input
 string CheckStrInput(int command){
-    cout << "Please enter a number: ";
     string str_input;
+    cout << "Please enter a number: ";
     cin >> str_input;
     cout << endl;
-    if(!CheckNum(str_input)){
-        cout << "Error: no negative values. Return to Main Menu." << endl;
-        return "";
+    if(!(checkNum(str_input))){
+        cout << "Error: incorrect number format. No negative or decimal values" << endl;
+        return "-1";
     }
 
     if(command < 2){
-        bool flag = CheckBinary(str_input);
+        bool flag = checkBinary(str_input);
         if(!flag){
-            cout << "Flag: " << flag << " Error: incorrect binary format." << endl;
-            return "";
+            cout << "Error: incorrect binary format." << endl;
+            return "-1";
         }
     }
-
+    cin.clear();
     return str_input;
 }
 
-//CheckIntInput function takes command input, checks for error, if found return 0, else returns num_input 
+//CheckIntInput function takes command input, checks for error, if found return -1, else returns num_input 
 int CheckIntInput(int command){
-    cout << "Please enter a number: ";
     int num_input;
+    cout << "Please enter a number: ";
     cin >> num_input;
-    cout << endl;
     if(cin.fail()){
         cin.clear();
         cin.ignore();
-        cout <<"Error: not an integer value." << endl;
-        return 0;
+        cout << "Error: not an integer." << endl;
+        return -1;
     }
-    
-    if(num_input < 0){
-        cout << "Error: no negative values." << endl;
-        return 0;
+    cout << endl;
+    if(num_input < 1){
+        cout << "Error: no numbers less than 1." << endl;
+        return -1;
     }
+
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
     return num_input;
 }
 
-//ExecuteCommand function takes command input, finds the function matching command, and returns result
+//ExecuteCommand function takes command input, finds the function matching command, and output result if no errors
 void ExecuteCommand(int command){
     string result = "";
+    cout << endl;
     switch(command){
     case 0 : {
         cout << "Binary to Decimal Conversion" << endl;
@@ -239,13 +258,15 @@ void ExecuteCommand(int command){
     }
 
     }
-
-    cout << "\n" << result << endl;
+    
+    if(result.length() > 0 && result != "-1")
+        cout << "\n" << result << endl;
 }
 
-//MenuCommand function shows the menu and sends command input to ExecuteCommand function
+//MenuCommand function shows the menu and sends input command to ExecuteCommand function
 void MenuCommand(){
     int command = 0;
+
     while(command != 6){
         cout << "\nEnter a number command from the menu \n0 Binary to Decimal \n1 Binary to Hexadecimal\n2 Decimal to Binary\n3 Hexadecimal to Decimal";
         cout << "\n4 Decimal to Hexadecimal\n5 Hexadecimal to Binary\n6 Quit" << endl;
